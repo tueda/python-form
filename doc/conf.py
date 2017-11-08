c03b664d37f4a96f2af96cb4e0ca29929f775ca1
Takahiro Ueda <tueda@nikhef.nl>
2017-11-08 20:54:45 +0100

    Fix a lint error "I202: Additional newline in a section of imports"

--- doc/conf.py
+++ doc/conf.py
@@ -3,13 +3,13 @@
 """Sphinx documentation build configuration."""
 
 import datetime
+import os
+import sys
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
 # documentation root, use os.path.abspath to make it absolute, like shown here.
 #
-import os
-import sys
 sys.path.insert(0, os.path.abspath('..'))
 
 # Import the package metadata from ../setup.py.
