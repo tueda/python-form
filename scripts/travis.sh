620d52a14532bd11e49bbf8b5b9d807d3ce4c64c
Takahiro Ueda <tueda@st.seikei.ac.jp>
2021-01-08 20:52:28 +0900

    fix: ci

--- scripts/travis.sh
+++ scripts/travis.sh
@@ -86,7 +86,8 @@ travis_test_install() {
   case "$TRAVIS_PYTHON_VERSION" in
     2.6)
       travis_retry pip install 'colorama<0.4.0' 'idna<2.8'
-      travis_retry pip install 'nose-timer<1.0.0'  # ??
+      # travis_retry pip install 'nose-timer<1.0.0'  # BAD ??
+      travis_retry pip install 'nose-timer<0.7.3'  # ??
       ;;
     3.2)
       travis_retry pip install 'coverage<4.0.0' 'coveralls<2.0.0'
@@ -95,8 +96,9 @@ travis_test_install() {
       travis_retry pip install 'colorama<0.4.2'
       ;;
     pypy)
-      #travis_retry pip install 'cryptography<3.0.0'  # ??
-      travis_retry pip install 'cryptography<3.1.0'  # ??
+      #travis_retry pip install 'cryptography<3.0.0'  # OK ??
+      # travis_retry pip install 'cryptography<3.1.0'  # OK ??
+      travis_retry pip install 'cryptography<3.3.0'  # ??
       ;;
   esac
   travis_retry pip install coveralls rednose nose-timer
